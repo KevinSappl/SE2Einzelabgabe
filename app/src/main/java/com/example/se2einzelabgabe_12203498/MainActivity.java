35a0986c72288dface637460df997636f8f685cb
KevinSappl <sappl.kevke@gmx.at>
2024-03-02 16:01:06 +0100

    2.1 Implemented Matrikelnummer TCP Example

--- app/src/main/java/com/example/se2einzelabgabe_12203498/MainActivity.java
+++ app/src/main/java/com/example/se2einzelabgabe_12203498/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.se2einzelabgabe_12203498;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 public class MainActivity extends AppCompatActivity {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String ip = "se2-submission.aau.at";
+    private int port = 20080;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void startConnection(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        clientSocket.setSoTimeout(10000);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public void sendMatNrToServer(View view) {
+        new Thread(() -> {
+            try {
+                startConnection(ip, port);
+
+                final EditText matNrInput = findViewById(R.id.matNrInput);
+                String matNrToSend = matNrInput.getText().toString();
+
+                out.println(matNrToSend);
+
+                String message = in.readLine();
+
+                runOnUiThread(() -> {
+                    TextView serverOutput = findViewById(R.id.serverOutput);
+                    serverOutput.setText(message);
+                });
+            } catch (IOException ex) {
+                runOnUiThread(() -> {
+                    TextView serverOutput = findViewById(R.id.serverOutput);
+                    serverOutput.setText(ex.getMessage());
+                });
+            }
+        }).start();
     }
 }
